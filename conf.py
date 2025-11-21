32293cfe7efca8120dbb86b6ca92a0bbd77c81d2
Kensuke Mochizuki <kmochizuki@quemix.com>
2025-11-21 16:08:16 +0900

    マニュアル改訂日を修正しました。

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Quloud Ver.6.0 Manual'
-copyright = '2025, Quemix Inc.　Last updated on Nov 20, 2025'
+copyright = '2025, Quemix Inc.　Last updated on Nov 21, 2025'
 author = 'Quemix inc'
 version = 'ver. 6.0.0'
 
