8f8a74ed27ef9f94fb9952e0ca5b31d47d88ad57
Kensuke Mochizuki <kmochizuki@quemix.com>
2026-03-17 12:40:07 +0900

    第１２章の Monte Carlo の記述を修正、第１４章の変更履歴と注意事項を追記しました。

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Quloud Ver.6.1.0 Manual'
-copyright = '2026, Quemix Inc.　Last updated on Mar 16, 2026'
+copyright = '2026, Quemix Inc.　Last updated on Mar 17, 2026'
 author = 'Quemix inc'
 version = '2026.3.16'
 
